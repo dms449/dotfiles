a322e0cb8a6998cf42a9b9aac25839d75a61b8ee
Daniel Stevenson <stevensondm23@gmail.com>
2019-07-07 21:19:16 -0500

    Add VScode setup

---
+++ install/vscode_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bas
+
+printf "Setting up vscode\n"
+
+# install
+if $INSTALL; then 
+  printf "\t Installing vscode and dependencies...\n"
+  # install some dependencies
+  sudo apt install software-properties-common apt-transport-https wget
+
+  # import Microsoft GPG key 
+  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+  # enable the Visual Studio Code repository
+  sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+  # install vscode
+  sudo apt install code
+
+fi
+
+cd "${DOTFILES_HOME}/dotfiles/vscode"
+
+# symlink files
+files=("Code/")
+printf "\tSymlinking vscode files...\n"
+for f in ${files[@]}; do
+    if [ "$f" != ".." ] && [ "$f" != "." ] && [ "$f" != ".git*" ]; then 
+      symlink $(readlink -e "$f") "${HOME}/.config/$f"
+    fi
+done
+
+cd "$DOTFILES_INSTALL"
+
