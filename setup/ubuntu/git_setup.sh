7580501807ae092321dd4c2e0b069daa76336b63
dms449 <stevensondm23@gmail.com>
2023-02-06 22:33:30 -0600

    add github cli stuff

--- setup/ubuntu/git_setup.sh
+++ setup/ubuntu/git_setup.sh
@@ -6,20 +6,15 @@ printf "=============================================================\n"
 
 # install
 if $INSTALL; then
-  # nothing to install
-  echo "nothing to install."
+  sudo apt install gh
 fi
 
 
 cd "${DOTFILES_HOME}/dotfiles/git"
 # symlink files
-files=(".gitconfig")
-printf "\tSymlinking git files...\n"
-for f in ${files[@]} ; do
-  if [ "$f" != ".." ] && [ "$f" != "." ] && [ "$f" != ".git*" ]; then 
-    symlink $(readlink -e "$f") "${HOME}/$f"
-  fi
-done
+
+symlink $(readlink -e "config.yml") "${HOME}/.config/gh/config.yml"
+symlink $(readlink -e ".gitconfig") "${HOME}/.gitconfig"
 
 printf "=================== git setup complete ====================\n"
 cd "$DOTFILES_SETUP"
