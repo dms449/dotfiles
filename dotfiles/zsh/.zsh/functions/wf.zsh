91c0c70c3651492d556fa06dd16d7fff96186ebd
dms449 <stevensondm23@gmail.com>
2023-03-23 13:27:00 -0500

    updated wf

--- dotfiles/zsh/.zsh/functions/wf.zsh
+++ dotfiles/zsh/.zsh/functions/wf.zsh
@@ -1,18 +1,33 @@
-function card() {
+card() {
   wf --show-card --card $(git rev-parse --abbrev-ref HEAD | read branch; echo ${${branch[(ws:/:)2]}[(ws:-:)1]})
 }
 
-function finished() {
+finished() {
   CARD=$(git rev-parse --abbrev-ref HEAD | sed 's:.*/::' | sed 's/-.*//')
   wf --finish-card --card $CARD
 }
 
-function next_story() {
-  wf --start-card --card $(wf --list-unstarted-cards | fzf-tmux | awk '{print $1;}')
+next_story() {
+  wf --start-card --card $(wf --list-unstarted-cards | fzf-tmux | awk '{print $1;}') --include-related
 }
 
-function testing() {
+testing() {
   CARD=$(git rev-parse --abbrev-ref HEAD | sed 's:.*/::' | sed 's/-.*//')
   wf --create-testing-lists --card $CARD
 }
 
+buildrelease() {
+  if [[ $# > 0 ]]; then
+    wf --approved-prs | fzf -m | cut -d' ' -f1 | tr '\n' ',' | xargs wf --build-release --list $1 --pull-requests
+  else
+    echo "argument required (release name)"
+  fi
+}
+
+deploy() {
+  if [[ $# > 0 ]]; then
+    wf --deploy --list $1
+  else
+    echo "argument required (release name)"
+  fi
+}
