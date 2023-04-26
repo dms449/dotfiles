57db7511548b34b6245b10b56a95400ab34a4c53
dms449 <stevensondm23@gmail.com>
2023-04-26 09:18:32 -0500

    use new vim keymap command and create folding toggle command

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,11 +1,3 @@
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 vim.api.nvim_create_user_command(
   'FileManager',
   function()
--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -1,24 +1,36 @@
+local function toggle_folds()
+    local get_opt = vim.api.nvim_win_get_option
+    local set_opt = vim.api.nvim_win_set_option
+
+    if get_opt(0, "foldlevel") >= 20 then
+        set_opt(0, "foldlevel", 4)
+    else
+        set_opt(0, "foldlevel", 20)
+    end
+end
+
 -- searching
-map("n", "<leader>P", ":PFiles<CR>")
-map("n", "<leader>p", ":GitFiles<CR>")
-map("n", "<leader>og", ":GitFiles?<CR>")
-map("n", "<leader>fw", ":Find <C-r><C-w><CR>")
-map("n", "<leader>ff", ":Find <C-r> ")
-map("n", "<leader>b", ":Buffers<CR>")
-map("n", "<leader>t", ":Tags<CR>")
-map("n", "<leader>-", ":split | :Files<CR>")
-map("n", "<leader>\\", ":vsplit | :Files<CR>")
-map("n", "<leader>fy", ":Find <c-R>\"<CR>")
-map("n", "<leader>lf", ":FileManager<CR>")
+vim.keymap.set("n", "<leader>P", ":PFiles<CR>")
+vim.keymap.set("n", "<leader>p", ":GitFiles<CR>")
+vim.keymap.set("n", "<leader>og", ":GitFiles?<CR>")
+vim.keymap.set("n", "<leader>fw", ":Find <C-r><C-w><CR>")
+vim.keymap.set("n", "<leader>ff", ":Find <C-r> ")
+vim.keymap.set("n", "<leader>b", ":Buffers<CR>")
+vim.keymap.set("n", "<leader>t", ":Tags<CR>")
+vim.keymap.set("n", "<leader>-", ":split | :Files<CR>")
+vim.keymap.set("n", "<leader>\\", ":vsplit | :Files<CR>")
+vim.keymap.set("n", "<leader>fy", ":Find <c-R>\"<CR>")
+vim.keymap.set("n", "<leader>lf", ":FileManager<CR>")
 
 -- moving
-map("n", "<M-k>", ":m-2<CR>")
-map("n", "<M-j>", ":m+<CR>")
+vim.keymap.set("n", "<M-k>", ":m-2<CR>")
+vim.keymap.set("n", "<M-j>", ":m+<CR>")
 
-map("n", "<leader>rr", ":%s/\\C<C-r><C-w>/")
+vim.keymap.set("n", "<leader>rr", ":%s/\\C<C-r><C-w>/")
 
 -- switch panes
-map("n", "<CR>", "<CR>:noh<CR>")
+vim.keymap.set("n", "<CR>", "<CR>:noh<CR>")
 
 -- folding
-map("n", "<leader>z", "zR")
+vim.keymap.set("n", "<leader>u", toggle_folds)
+
