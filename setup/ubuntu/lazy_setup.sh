f862a7763f5684e0c0412e8e647110ec9790a407
dms449 <stevensondm23@gmail.com>
2023-10-03 06:17:37 -0500

    rm lazygit artifacts after installing

--- setup/ubuntu/lazy_setup.sh
+++ setup/ubuntu/lazy_setup.sh
@@ -11,6 +11,7 @@ if $INSTALL; then
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit /usr/local/bin
+  rm lazygit.tar.gz lazygit
 
   curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 fi
