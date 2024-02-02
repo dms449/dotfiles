481232708eb037aff8dabfa6464e60637522f761
dms449 <stevensondm23@gmail.com>
2024-02-01 22:36:32 -0600

    add '.local' to synology nas

--- dotfiles/zsh/.zsh/functions/nfs.zsh
+++ dotfiles/zsh/.zsh/functions/nfs.zsh
@@ -5,6 +5,6 @@ mount_nfs () {
   sudo chown $USER:$USER /nfs/$directory_name
   sudo chmod a+rwx /nfs/$directory_name
 
-  echo "SynologyDS716:/volume1/$directory_name /nfs/$directory_name nfs defaults 0 0" | sudo tee -a /etc/fstab
+  echo "SynologyDS716.local:/volume1/$directory_name /nfs/$directory_name nfs defaults 0 0" | sudo tee -a /etc/fstab
   sudo mount -a
 }
