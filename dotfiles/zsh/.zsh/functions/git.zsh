f39c3b0642e8c9b02fd39b2969a334ff97c428d2
dms449 <stevensondm23@gmail.com>
2024-06-05 12:52:07 -0500

    default to "Ready For Review"

--- dotfiles/zsh/.zsh/functions/git.zsh
+++ dotfiles/zsh/.zsh/functions/git.zsh
@@ -153,7 +153,7 @@ cherry() {
 
 prd() {
   issue_id=$(current_branch | grep -o -P 'BW-(\d*)')
-  gh pr create -R="BaldwinAviation/baldwin-web" -B=develop -l="Ready for Walkthrough" -t="$(current_branch)" -b="https://portsideco.atlassian.net/browse/$issue_id"
+  gh pr create -R="BaldwinAviation/baldwin-web" -B=develop -l="Ready For Review" -t="$(current_branch)" -b="https://portsideco.atlassian.net/browse/$issue_id"
 }
 
 changed_files() {
