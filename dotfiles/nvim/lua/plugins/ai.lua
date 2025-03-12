3452b229bb74375220bb55a587fedbebe8264635
dms449 <stevensondm23@gmail.com>
2025-03-11 21:09:42 -0500

    back to codium

--- dotfiles/nvim/lua/plugins/ai.lua
+++ dotfiles/nvim/lua/plugins/ai.lua
@@ -1,8 +1,12 @@
 return {
- {
-    "supermaven-inc/supermaven-nvim",
+{
+    "Exafunction/codeium.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
     config = function()
-      require("supermaven-nvim").setup({})
-    end,
-  },
+        require("codeium").setup({})
+    end
+},
 }
