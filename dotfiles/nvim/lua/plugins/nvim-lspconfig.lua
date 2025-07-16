c503955bf55a50bce3f56a9fc4e2efca50f74bf4
dms449 <stevensondm23@gmail.com>
2025-07-15 21:36:19 -0500

    replace tsserver with ts_ls

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -74,7 +74,7 @@ return {
 
       -- Use a loop to conveniently call 'setup' on multiple servers and
       -- map buffer local keybindings when the language server attaches
-      local servers = {'eslint', 'bashls', 'pyright', 'julials', 'html', 'tsserver', 'solargraph', 'svelte'}
+      local servers = {'eslint', 'bashls', 'pyright', 'julials', 'html', 'ts_ls', 'solargraph', 'svelte'}
 
       -- Set settings for language servers below
       --
