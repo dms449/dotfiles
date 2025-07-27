d1fb156e800a2aad631f11d000b91875fb90415c
dms449 <stevensondm23@gmail.com>
2025-07-27 15:08:25 -0500

    nvim terminal config

--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -1,12 +1,12 @@
 local function toggle_folds()
-    local get_opt = vim.api.nvim_win_get_option
-    local set_opt = vim.api.nvim_win_set_option
+  local get_opt = vim.api.nvim_win_get_option
+  local set_opt = vim.api.nvim_win_set_option
 
-    if get_opt(0, "foldlevel") >= 20 then
-        set_opt(0, "foldlevel", 4)
-    else
-        set_opt(0, "foldlevel", 20)
-    end
+  if get_opt(0, "foldlevel") >= 20 then
+    set_opt(0, "foldlevel", 4)
+  else
+    set_opt(0, "foldlevel", 20)
+  end
 end
 
 -- searching
@@ -16,7 +16,6 @@ vim.keymap.set("n", "<leader>og", ":GitFiles?<CR>")
 vim.keymap.set("n", "<leader>fw", ":Find <C-r><C-w><CR>")
 vim.keymap.set("n", "<leader>ff", ":Find <C-r> ")
 vim.keymap.set("n", "<leader>b", ":Buffers<CR>")
-vim.keymap.set("n", "<leader>t", ":Tags<CR>")
 vim.keymap.set("n", "<leader>-", ":split | :Files<CR>")
 vim.keymap.set("n", "<leader>\\", ":vsplit | :Files<CR>")
 vim.keymap.set("n", "<leader>fy", ":Find <c-R>\"<CR>")
@@ -30,7 +29,56 @@ vim.keymap.set("n", "<leader>rr", ":%s/\\C<C-r><C-w>/")
 
 -- switch panes
 vim.keymap.set("n", "<CR>", "<CR>:noh<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
 
 -- folding
 vim.keymap.set("n", "<leader>u", toggle_folds)
 
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end
+})
+-- terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- terminal buffer tracking
+local terminal_buf = nil
+
+vim.keymap.set("n", "<leader>t", function()
+  -- Check if terminal buffer exists and is valid
+  if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
+    -- Find if terminal is already open in a window
+    local terminal_win = nil
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == terminal_buf then
+        terminal_win = win
+        break
+      end
+    end
+    
+    if terminal_win then
+      -- Terminal is already open, just focus it and enter terminal mode
+      vim.api.nvim_set_current_win(terminal_win)
+      vim.cmd.startinsert()
+    else
+      -- Terminal buffer exists but not visible, open it in new window
+      vim.cmd.vnew()
+      vim.api.nvim_win_set_buf(0, terminal_buf)
+      vim.cmd.wincmd("J")
+      vim.api.nvim_win_set_height(0, 15)
+      vim.cmd.startinsert()
+    end
+  else
+    -- Create new terminal buffer
+    vim.cmd.vnew()
+    vim.cmd.term()
+    terminal_buf = vim.api.nvim_get_current_buf()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+    vim.cmd.startinsert()
+  end
+end)
--- dotfiles/nvim/lua/plugins/tig.lua
+++ dotfiles/nvim/lua/plugins/tig.lua
@@ -3,7 +3,7 @@ return {
     "kthibodeaux/tig.vim",
     keys = {
       { '<leader>c', '<Plug>TigFileHistory' },
-      { '<leader>tb', '<Plug>TigBlame' },
+      { '<leader>T', '<Plug>TigBlame' },
       { '<leader>y', '<Plug>TigLatestCommitForLine' },
     }
   }
