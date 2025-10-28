93cc596112af2a0e655ef9665759be6ca4929fd7
dms449 <stevensondm23@gmail.com>
2025-10-28 07:51:44 -0500

    fix outdated lsp setup

--- mac/dotfiles/nvim/lua/plugins/lsp.lua
+++ mac/dotfiles/nvim/lua/plugins/lsp.lua
@@ -24,78 +24,23 @@ return {
     },
     config = function()
       local mason_lspconfig = require("mason-lspconfig")
-      local lspconfig = require("lspconfig")
-
-      local vue_language_server_path = vim.fn.stdpath('data') ..
-          "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-      local vue_plugin = {
-        name = '@vue/typescript-plugin',
-        location = vue_language_server_path,
-        languages = { 'vue', 'typescript', 'javascript' },
-        configNamespace = 'typescript',
+      ensure_installed = {
+        "lua_ls",
+        "bashls",
+        "pyright",
+        "julials",
+        "postgres_lsp",
+        "vtsls",
+        "dockerls",
+        "marksman",
       }
-      local vtsls_config = {
-        settings = {
-          vtsls = {
-            tsserver = {
-              globalPlugins = {
-                vue_plugin,
-              },
-            },
-          },
-        },
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-      }
-
-      local vue_ls_config = {
-        on_init = function(client)
-          client.handlers['tsserver/request'] = function(_, result, context)
-            local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
-            if #clients == 0 then
-              vim.notify('Could not find `vtsls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
-              return
-            end
-            local ts_client = clients[1]
-
-            local param = unpack(result)
-            local id, command, payload = unpack(param)
-            ts_client:exec_cmd({
-              title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-              command = 'typescript.tsserverRequest',
-              arguments = {
-                command,
-                payload,
-              },
-            }, { bufnr = context.bufnr }, function(_, r)
-              local response_data = { { id, r.body } }
-              ---@diagnostic disable-next-line: param-type-mismatch
-              client:notify('tsserver/response', response_data)
-            end)
-          end
-        end,
-      }
-      -- nvim 0.11 or above
-      vim.lsp.config('vtsls', vtsls_config)
-      vim.lsp.config('vue_ls', vue_ls_config)
-      vim.lsp.enable({ 'vtsls', 'vue_ls' })
-
       -- Ensure these servers are installed
       mason_lspconfig.setup({
-        ensure_installed = {
-          "lua_ls",
-          "bashls",
-          "pyright",
-          "julials",
-          "postgres_lsp",
-          "vue_ls",
-          "vtsls",
-          "dockerls",
-          "marksman",
-          "vtsls",
-        },
+        ensure_installed = ensure_installed,
         automatic_installation = true,
-        automatic_enable = false,
       })
+
+
 
       -- Add additional capabilities supported by nvim-cmp
       local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -115,9 +60,6 @@ return {
         },
       }
 
-      -- Use an on_attach function to only map the following keys
-      -- after the language server attaches to the curcreate
-      -- Enable completion triggered by <c-x><c-o>
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
@@ -142,12 +84,6 @@ return {
         end
       })
 
-      local servers = { 'lua_ls', 'bashls', 'pyright', 'julials', 'postgres_lsp', 'vtsls', 'dockerls' }
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {}
-      end
-      -- vim.lsp.enable({ 'vtsls', 'vue_ls' })
-
       -- Configure diagnostics
       vim.diagnostic.config({
         virtual_text = {
@@ -161,6 +97,38 @@ return {
         update_in_insert = false,
         severity_sort = true,
       })
+
+      -- Configure each LSP server from ensure_installed
+      for _, server in ipairs(ensure_installed) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+      end
+
+
+      local vue_language_server_path = vim.fn.stdpath('data') ..
+            "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+      vim.lsp.config('vtsls', {
+        capabilities = capabilities,
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = '@vue/typescript-plugin',
+                  location = vue_language_server_path,
+                  languages = { 'vue' },
+                },
+              },
+            },
+          },
+        },
+      })
+
+
+
     end,
   },
 }
